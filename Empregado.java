2d5ee63c0dbb9066bcbc7f9ca09c24ea6b7d2747
Guilherme Vieira de Borba - 821136842 <821136842@ulife.com.br>
2024-02-28 20:08:49 -0300

    Segundo commit. Classe Empregado atualizado

--- Empregado.java
+++ Empregado.java
@@ -4,16 +4,19 @@ public class Empregado {
     private int idade;
 
     public String getNome() {
-        return nome;
+        return nome; 
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public int getIdade() {
         return idade;
     }
+
     public void setIdade(int idade) {
         this.idade = idade;
     }
-    
+
 }
